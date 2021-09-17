b0dd4eb0c7f5930b1185d70571d492bef52f5f1f
shashwat tiwari <shashwattiwari8@gmail.com>
2021-09-17 15:17:36 +0530

    Added custom linkedList class

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -2,23 +2,33 @@ public class Main{
   public static void main(String args[])
   {
 
-    Stack stack = new Stack(3);
-       stack.push(1);
-       stack.push(3);
-       stack.push(2);
+    // Stack stack = new Stack(3);
+    //    stack.push(1);
+    //    stack.push(3);
+    //    stack.push(2);
+    //
+    //    stack.printStack();
+    //    // remove
+    //    stack.pop();
+    //
+    // Queue queue = new Queue(3);
+    //        queue.enQueue(1);
+    //        queue.enQueue(3);
+    //        queue.enQueue(6);
+    //
+    //        // remove
+    //        queue.deQueue();
+    //
+    //        queue.printQueue();
+           // Linked List
 
-       stack.printStack();
-       // remove
-       stack.pop();
+                  LinkedList linkedList = new LinkedList();
+                  linkedList.insertAtEnd(1);
+                  linkedList.insertAtFront(3);
+                  linkedList.insertAtFront(10);
+                  linkedList.insert(linkedList.head.next,9);
 
-    Queue queue = new Queue(3);
-           queue.enQueue(1);
-           queue.enQueue(3);
-           queue.enQueue(6);
+                  linkedList.printList();
 
-           // remove
-           queue.deQueue();
-
-           queue.printQueue();
   }
 }
