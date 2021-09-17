ac427fa0b6df5bd1e5c0f37e7a60091fd1584c8d
shashwat tiwari <shashwattiwari8@gmail.com>
2021-09-17 12:06:46 +0530

    Stack Implementation

---
+++ Assignment/Stack.java
@@ -0,0 +1,57 @@
+public class Stack
+{
+  private int arr[];
+  private int capacity;
+  private int top;
+
+  public Stack(int length)
+  {
+    if(length>0)
+    {
+      this.arr = new int[length];
+      this.top = -1;
+      this.capacity = length;
+    }
+  }
+
+  public boolean isFull()
+  {
+    return top==capacity-1;
+  }
+
+  public boolean isEmpty()
+  {
+    return top==-1;
+  }
+
+  public int size()
+  {
+    return top+1;
+  }
+
+  public void push(int element)
+  {
+    if(!isFull())
+    {
+      arr[++top]=element;
+    }
+  }
+
+  public int pop()
+  {
+    if(isEmpty())
+    {
+      System.exit(1);
+    }
+    return arr[top--];
+  }
+
+  public int peek()
+  {
+    if(!isEmpty())
+    {
+      return arr[top];
+    }
+    return -1;
+  }
+}
