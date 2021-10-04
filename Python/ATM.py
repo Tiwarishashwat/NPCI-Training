df63a26581885d1ee6f778591549a3dadae0534b
shashwat tiwari <shashwattiwari8@gmail.com>
2021-10-04 13:33:07 +0530

    Assignment for Monday done

--- Python/ATM.py
+++ Python/ATM.py
@@ -1,3 +1,16 @@
+from os import system, name
+# import sleep to show output for some time period
+from time import sleep
+# define our clear function
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 class ATM():
     def __init__(self, currency, limit, timesWithdraw):
         self.currency = currency
@@ -18,33 +31,46 @@ class Account:
         self.transactionType = transactionType
         self.balance = 50000
 
-    def debit(self,amount,maxAmount,obj):
-        if amount > self.balance:
+    def debit(self,Oamount,maxAmount,obj):
+        if Oamount > self.balance:
             return "Insufficient Balance Amount"
-        elif amount > maxAmount:
+        elif Oamount > maxAmount:
             return "Transaction amount unavailable. Please enter smaller amount"
         else:
+            amount = Oamount
             count500=0
             while amount>=500 and count500 < obj.currency[500]:
                 count500+=1;
                 amount=amount-500
-            print("count of 500", count500)
+
             count200=0
             while amount>=200 and count200 < obj.currency[200]:
                 count200+=1
                 amount=amount-200
-            print("count of 200", count200)
+
             count100=0
             while amount>=100 and count100 < obj.currency[100]:
                 count100+=1
                 amount=amount-100
-            print("count of 100", count100)
-            if amount > count100*100 + count200*200 + count500*500:
-                return "Dispense currency not accurate, Please enter multiples of 500, 200 or 100"
+
+            wdAmount = count100*100 + count200*200 + count500*500
+
+            if Oamount > wdAmount:
+                print("This amount cannot be Dispensed. Do you want to Withdraw ",count100*100+count200*200+count500*500, "Yes/No")
+                choice=input()
+                if choice == "Yes":
+                    obj.currency[500]-=count500
+                    obj.currency[200]-=count200
+                    obj.currency[100]-=count100
+                    self.balance-=Oamount
+                    return "Debit Transaction Successful!"
+                elif choice == "No":
+                    return "Dispense currency not accurate, Please enter multiples of 500, 200 or 100"
             else:
                 obj.currency[500]-=count500
                 obj.currency[200]-=count200
                 obj.currency[100]-=count100
+                self.balance-=Oamount
                 return "Debit Transaction Successful!"
 
     def credit(self, den_100,den_200,den_500,obj):
@@ -56,9 +82,42 @@ class Account:
         return "Credit Successful!"
 
 atm = ATM(currency,5000,2)
-print(atm.currency)
-account = Account(10501,"debit")
-print(account.debit(15500,0.9*atm.calMaxWithdrawAmount(),atm))
-print(atm.currency)
-print(account.credit(10,10,10,atm))
-print(atm.currency)
+while True:
+    print("\t --------------- \t Hi Welcome to xyz ATM \t --------------- ")
+
+    print("Enter your bank account")
+    accNo = input()
+
+    print("Enter your bank pincode")
+    pinCode = input()
+    if pinCode!="2222":
+        print("Wrong pin code, this action will be reported!")
+        break
+
+    print(".....Press 1 for credit")
+    print(".....Press 2 for debit")
+    print(".....Press 3 for exit")
+    choice = input()
+
+    if choice == "1":
+        account = Account(accNo,"credit")
+        print("Enter Denomination of 100")
+        den100 = int(input())
+        print("Enter Denomination of 200")
+        den200 = int(input())
+        print("Enter Denomination of 500")
+        den500 = int(input())
+        print(account.credit(den100,den200,den500,atm))
+        sleep(3)
+        clear()
+
+
+    elif choice== "2":
+        account = Account(accNo,"debit")
+        print("Available Currency is: ",atm.currency)
+        print(account.debit(520,0.9*atm.calMaxWithdrawAmount(),atm))
+        sleep(3)
+        clear()
+    else:
+        print("Thankyou for using xyz ATM")
+        break
